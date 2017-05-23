4e4529ee42dc8e184c7e7be4b263a77a584d9329
mortenlerudjordet <git@lerun.info>
2017-05-23 08:48:34 +0200

    Minor changes

--- SCOM/MPs/Microsoft.SystemCenter.2007.Addendum/WMIFunctionalCheck.ps1
+++ SCOM/MPs/Microsoft.SystemCenter.2007.Addendum/WMIFunctionalCheck.ps1
@@ -1,5 +1,4 @@
 ﻿#Copyright (c) Microsoft Corporation. All rights reserved.
-# Addendum by: Morten Lerudjordet
 #*************************************************************************
 # $ScriptName:  "WMIFunctionalCheck" $
 #
